d4412365ff63f0c85cf2c07b484c0dcf8b86bbf2
zaptoe <101872189+zaptoe@users.noreply.github.com>
2022-03-22 01:25:17 -0500

    Create search.h

---
+++ src/search.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <ctime>
+#include "defs.h"
+
+enum Flag { UPPER, LOWER, EXACT };
+
+class Search
+{
+  public:
+  	Search(Evaluation evaluation);
+  	Value go(std::time_t timeAllowed);
+  private:
+	  Value search(Board board, Value alpha, Value beta, Depth depth, int ply = 1);
+	  Value qsearch(Board board, Value alpha, Value beta, int ply = 1);
+};
