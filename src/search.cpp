0a6f9830c12639c31470038324e4eab7d4b23f86
Zaptoe Chess Engine <101872189+zaptoe@users.noreply.github.com>
2022-03-22 02:03:48 -0500

    Create search.cpp

---
+++ src/search.cpp
@@ -0,0 +1,23 @@
+#include <ctime>
+#include "defs.h"
+#include "evaluation.cpp"
+
+Search::Search(Evaluation evaluation)
+{
+
+};
+
+Move Search::go(Board board, std::time_t timeAllowed)
+{
+
+};
+
+Value Search::search(Board board, Value alpha, Value beta, Depth depth, int ply = 1)
+{
+
+};
+
+Value Search::qsearch(Board board, Value alpha, Value beta, int ply = 1)
+{
+
+};
