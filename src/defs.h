b2594a344a476badc218e9cbe3d3a9a5b35dacaf
zaptoe <101872189+zaptoe@users.noreply.github.com>
2022-03-22 01:24:40 -0500

    Create defs.h

---
+++ src/defs.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <limits>
+#include <string>
+#include "board.h"
+#include "evaluation.h"
+#include "search.h"
+
+const int INF = std::numeric_limits<int>::max();
+
+enum BoardSize { SMALL, LARGE };
+enum Color { RED, BLUE, YELLOW, GREEN };
+enum PieceType { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING };
+enum Variants { REGULAR, ANTICHESS, NCHECK, ATOMIC };
+
+typedef int Depth;
+typedef int Value;
+
+struct Move {
+  int from;
+  int to;
+  int bit;
+};
+
+struct Entry {
+  Value score;
+  Depth depth;
+  Flag flag;
+};
